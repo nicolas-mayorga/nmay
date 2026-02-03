841cb1add8ece6626ce9d784a70a9aa847c0af14
nicolas-mayorga <nmayorgar@gmail.com>
2026-02-03 15:50:47 -0500

    added output to see open ports, also program now attempts to get a banner from the open service

--- src/core/scanner.py
+++ src/core/scanner.py
@@ -1,9 +1,28 @@
 from network.tcp import tcp_connect_scan
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def multithread_tcp_scan(target, ports, timeout, threads=10):
+    open_ports = []
+    with ThreadPoolExecutor(max_workers=threads) as executor:
+        futures = []
+
+        for port in ports:
+            futures.append(executor.submit(tcp_connect_scan, target, port, timeout))
+
+        for future in as_completed(futures):
+            port, banner, is_open = future.result()
+            print(banner)
+            if is_open:
+                open_ports.append(port)
+    
+    return open_ports
 
 def run_tcp_scan(target, ports, timeout):
-
-    for port in ports:
-        open = tcp_connect_scan(target, port, timeout)
-
-        if open:
-            print(f"{port} OPEN")
+    open_ports = multithread_tcp_scan(target, ports, timeout)
+    
+    print(f"{'PORT':<10} {'STATUS':<10} {'SERVICE'}")
+    print("=" * 30)
+    
+    for port in open_ports:
+        service = "placeholder" 
+        print(f"{str(port) + '/tcp':<10} {'open':<10} {service}")
--- src/network/tcp.py
+++ src/network/tcp.py
@@ -4,13 +4,20 @@ import socket
 def tcp_connect_scan(host, port, timeout):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(timeout)
-
+    is_open = False
+    banner = ""
     try:
-        s.connect((host, port))
-        return True
+        is_open = not s.connect_ex((host, port))
+        banner = s.recv(1024).decode(errors='ignore').strip() # check to see if there's a banner on the open port
+        
+
+        if not banner:
+            s.send(b"GET / HTTP/1.1\r\nHost: " + host.encode() + b"\r\n\r\n") # if not, send a generic http request to see if anything happens
+            banner = s.recv(1024).decode(errors='ignore').strip()
+        
     except:
-        return False
+        banner = "Unknown/no banner"
     finally:
         s.close()
-
+    return port, banner, is_open
         
